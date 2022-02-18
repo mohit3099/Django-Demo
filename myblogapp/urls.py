6b6a254691a3aee501c30f40e7bb676decedb8a0
mohit3099 <61134058+mohit3099@users.noreply.github.com>
2022-02-18 18:29:10 +0530

    Update urls.py

--- myblogapp/urls.py
+++ myblogapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post_edit/<int:pk>/', views.post_edit, name = 'blog-post-edit'),
     path('post_delete/<int:pk>/', views.post_delete, name = 'blog-post-delete'),
     path('myblogs', views.myblogs, name = 'myblogs'),
+    path('like/<int:pk>', views.like_view, name='like_post'),
 ]
