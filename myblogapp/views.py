3f8661da1ecafa3797ca52d7c9a5d9df83120fe8
mohit3099 <61134058+mohit3099@users.noreply.github.com>
2022-02-18 18:28:19 +0530

    Update views.py

--- myblogapp/views.py
+++ myblogapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import redirect, render
+from turtle import pos
+from django.shortcuts import redirect, render, get_object_or_404
 from django import forms
-from django.http.response import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
 from .models import PostModel
 from .forms import CommentForm, PostModelForm, PostUpdateForm
 from django.contrib.auth.decorators import login_required
@@ -30,6 +32,7 @@ def hello(request):
 @login_required
 def post_details(request,pk):
     post = PostModel.objects.get(id=pk)
+    
     if request.method == 'POST':
         c_form = CommentForm(request.POST)
         if c_form. is_valid():
@@ -40,11 +43,31 @@ def post_details(request,pk):
             return redirect('blog-post-details', pk = post.id)
     else:
         c_form = CommentForm()
+        total_likes = post.total_likes
+        liked = False
+        if post.likes.filter(id=request.user.id).exists():
+            liked = True
+        
     context = {
         'post' : post,
-        'c_form' : c_form
+        'c_form' : c_form,
+        'total_likes' : total_likes,
+        'liked' : liked
     }
     return render(request, 'demo/post_details.html', context)
+
+@login_required
+def like_view(request, pk):
+    post = get_object_or_404(PostModel, id=request.POST.get('post_id'))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+        
+    return HttpResponseRedirect(reverse('blog-post-details', args=[str(pk)]))
 
 @login_required
 def post_edit(request, pk):
